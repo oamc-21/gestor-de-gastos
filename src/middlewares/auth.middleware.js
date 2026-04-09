7208d21b66e49a6703e60997aea2991c826cb48b
Otto <dottmzc@gmail.com>
2026-04-08 21:15:29 -0600

    Implementacion de middleware para autenticacion & proteccion de endpoints por token

---
+++ src/middlewares/auth.middleware.js
@@ -0,0 +1,21 @@
+const jwt = require('jsonwebtoken');
+
+const authMiddleware = (req, res, next) => {
+    const authHeader = req.headers.authorization;
+    if (!authHeader || !authHeader.startsWith("Bearer ")) {
+        return res.status(401).json({ message: "No autorizado" });
+    }
+
+    try {
+        const token = authHeader.split(" ")[1];
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = decoded;
+        return next();
+
+    } catch (error) {
+        return res.status(401).json({message: error.message});
+    }
+
+}
+
+module.exports = authMiddleware;
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const { register, login } = require("../controllers/user.controller");
+const authMiddleware = require("../middlewares/auth.middleware");
 
+router.get("/me", authMiddleware, (req, res) =>{
+    res.status(200).json({message: "Ruta protegida OK", user: req.user});
+});
 router.post("/register", register);
 router.post("/login", login);
 
